2ddacf021687c4e715a826279ab76df60be407aa
Sean Raborg <sean.raborg@gmail.com>
2019-12-06 19:05:12 -0800

    Implemented add_transition method in AbstractWorld Implemented perform_action method in GridWorld
    Added corresponding documentation

--- src/worlds/abstract_world.py
+++ src/worlds/abstract_world.py
@@ -9,14 +9,10 @@ class AbstractWorld(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def __init__(self):
-        pass
+        self._state_actions = {}
 
     @property
     def states(self):
-        raise NotImplementedError
-
-    @property
-    def state_actions(self, state):
         raise NotImplementedError
 
     @property
@@ -30,6 +26,22 @@ class AbstractWorld(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def perform_action(self, state, action):
         pass;
+
+    ##
+    #   Add Transition Dynamics
+    #
+    #   @param values a tuple of the following format ( (state, action), (reward, [(probability, next_state)]) )
+    #
+    def add_transition(self, values):
+        total_probability = 0
+        for successor in values[1][1]:
+            total_probability += successor[0]
+
+        if total_probability != 1:
+            raise ValueError("Total probabilities should sum to 1")
+        else:
+            self._state_actions[(values[0][0], values[0][1])] = values[1]
+
 
     ##
     # Returns a list of all available states
--- src/worlds/grid_world.py
+++ src/worlds/grid_world.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import numpy as np
 
 from .abstract_world import AbstractWorld
 
@@ -16,5 +17,22 @@ class GridWorld(AbstractWorld, ABC):
     def run_policy(self, policy, iterations=1):
         pass
 
+    ##
+    # Determines the reward and resulting state after performing an action on the given state
+    #
+    # Actions selected are weighted using the probabilities defined in self._state_actions.get(state, action)
+    #
+    # @param state The starting state
+    # @param action The action to perform
+    # @return the resulting state
+    #
     def perform_action(self, state, action):
-        pass
+        reward = self._state_actions.get((state, action))[0]
+        successors = [s for s in self._state_actions.get((state, action))[1]]
+        probabilities, successor_states = zip(*successors)
+
+        successor_states = np.array(successor_states, dtype=object)
+
+        next_state = successor_states[np.random.choice(len(successor_states), p=probabilities)]
+        return next_state, reward
+

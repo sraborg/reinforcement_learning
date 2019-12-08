263661699b37b7017d34dd1571b3745e71832457
ThunderRoid <thunderroid@gmail.com>
2019-12-07 16:49:10 -0800

    hello1

--- src/policy_algorithms/policy_iteration.py
+++ src/policy_algorithms/policy_iteration.py
@@ -13,16 +13,25 @@ import numpy as np
 class PolicyIteration(implements(PolicyAlgorithm)):
     max_iterations = 1000
 
+    def policy_evaluation(self, value_table, world):
+        return 0
+
     def generate_policy(self, world):
         # mdp = world._state_actions
-
+        num_run = 0
         value_table = {}
         for state in world.get_states():
             value_table[state] = 0.0
 
-        print(value_table[(1, 1)])
+        for i in range(self.max_iterations):
+            is_stable = True
+            val_V = self.policy_evaluation(value_table, world)
 
-        z, x = world.perform_action((1, 1), world.Action.DOWN)
+            # Go through each state and try to improve actions that were taken (policy Improvement)
+            # for state in range(environment.nS):
+
+        print(value_table[(1, 1)])
+        z, x = world.perform_action((3, 7), world.Action.RIGHT)
         print(z)
         print(x)
 
